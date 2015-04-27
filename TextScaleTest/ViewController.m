abd1303a9d82d89cb5e987afc2fb9ebc7938432c
Brian Sharon <brian@floatplane.us>
2015-04-27 13:58:52 -0700

    scaling text experiment

--- TextScaleTest/ViewController.m
+++ TextScaleTest/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextView *right;
+@property (weak, nonatomic) IBOutlet UITextView *left;
+@property (weak, nonatomic) IBOutlet UISlider *slider;
 
 @end
 
@@ -17,11 +20,26 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.right.text = self.left.text;
+    self.right.minimumZoomScale = self.slider.minimumValue;
+    self.right.maximumZoomScale = self.slider.maximumValue;
+    self.right.delegate = self;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)sliderChanged:(id)sender {
+    //CGAffineTransform scale = CGAffineTransformMakeScale(self.slider.value, self.slider.value);
+    //self.right.transform = scale;
+    self.right.zoomScale = self.slider.value;
+}
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
+    return self.right.subviews[0];
 }
 
 @end
